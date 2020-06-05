8c1434daad8745a05a18041e6dcdf01b82c749e4
Anderson Pavani <anderson.pavani@neainformatica.com.br>
2020-06-05 17:09:56 -0400

    Commit Inicial

---
+++ src/main/java/br/com/andersonpavani/gtiservice/GtiServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersonpavani.gtiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtiServiceApplication.class, args);
+	}
+
+}
